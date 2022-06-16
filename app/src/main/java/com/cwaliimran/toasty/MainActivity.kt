c142cfaf7bd4965433304c1b083a533bd7840ec0
Ali Imran <cwaliimran@gmail.com>
2022-06-16 10:56:32 +0500

    Simple toast

---
+++ app/src/main/java/com/cwaliimran/toasty/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cwaliimran.toasty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cwaliimran.showtoast.showToast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showToast("Hello")
+    }
+}
